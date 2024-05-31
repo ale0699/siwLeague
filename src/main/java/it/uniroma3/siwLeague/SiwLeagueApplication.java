e178bf0cab1edaaf933b4d02c61f17c28e61dd2b
ale0699_PCDesktop <atulli030@gmail.com>
2024-05-31 10:34:01 +0200

    import iniziale

---
+++ src/main/java/it/uniroma3/siwLeague/SiwLeagueApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siwLeague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwLeagueApplication.class, args);
+	}
+
+}
