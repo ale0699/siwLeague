a31a20058739fa61a21929ed227a7203a2a8699b
ale0699_PCDesktop <atulli030@gmail.com>
2024-06-17 20:11:33 +0200

    aggiunti alcuni loghi e modifica a selectTorneo per visualizzare errori

--- src/main/java/it/uniroma3/siwLeague/controller/SquadraController.java
+++ src/main/java/it/uniroma3/siwLeague/controller/SquadraController.java
@@ -1,5 +1,6 @@
 package it.uniroma3.siwLeague.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import it.uniroma3.siwLeague.model.Squadra;
 import it.uniroma3.siwLeague.model.Torneo;
+import it.uniroma3.siwLeague.service.GiocatoreService;
 import it.uniroma3.siwLeague.service.SquadraService;
 import it.uniroma3.siwLeague.service.TorneoService;
 
@@ -23,12 +25,33 @@ public class SquadraController {
 	@Autowired
 	private SquadraService squadraService;
 	
+	@Autowired
+	private GiocatoreService giocatoreService;
+	
+	@GetMapping(value = "/squadra/{idSquadra}")
+	public String getSquadra(@PathVariable("idSquadra")Long idSquadra, Model model){
+		model.addAttribute("squadra", this.squadraService.findSquadraByIdSquadra(idSquadra));
+		model.addAttribute("giocatori", this.giocatoreService.findGiocatoriBySquadraIdSquadra(idSquadra));
+		return "squadra/squadra.html";
+	}
+	
 	@GetMapping(value = "/formAddSquadra/{idTorneo}")
-	public String getFormAddSquadra(@PathVariable("idTorneo")Long idTorneo, Model model) {
+	public String getFormAddSquadra(@PathVariable("idTorneo")Long idTorneo, Model model) throws Exception {
 		Torneo torneo = this.torneoService.getTorneoByIdTorneo(idTorneo);
-		model.addAttribute("torneo", torneo);
-		model.addAttribute(new Squadra());
-		return "squadra/formAddSquadra.html";
+		
+		//controllo che il torneo sia effettivamente aperto alle iscrizioni
+		if (torneo.isIscrizioneInCorso()) {
+			
+			model.addAttribute("torneo", torneo);
+			model.addAttribute(new Squadra());
+			return "squadra/formAddSquadra.html";
+		}
+		else {
+			model.addAttribute("error", "Non puoi iscrivere la tua squadra al torneo "+torneo.getNome());
+			model.addAttribute("torneiAperteIscrizioni", this.torneoService.findTorneiByIscrizioneInCorso(true));
+			model.addAttribute("torneiChiuseIscrizioni", this.torneoService.findTorneiByIscrizioneInCorso(false));
+			return "torneo/selectTorneo.html";
+		}
 	}
 	
 	@PostMapping(value = "addSquadra")
