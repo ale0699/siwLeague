ca21eb35d6083563efbc73b474ed81dc43cad965
ale0699_PCDesktop <atulli030@gmail.com>
2024-06-23 13:54:23 +0200

    nella dashboard vengono visualizzate le proprie squadre

--- src/main/java/it/uniroma3/siwLeague/controller/IndexController.java
+++ src/main/java/it/uniroma3/siwLeague/controller/IndexController.java
@@ -1,10 +1,15 @@
 package it.uniroma3.siwLeague.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import it.uniroma3.siwLeague.model.GestoreSquadra;
+import it.uniroma3.siwLeague.service.CredenzialiService;
+import it.uniroma3.siwLeague.service.GestoreSquadraService;
 import it.uniroma3.siwLeague.service.SquadraService;
 
 @Controller
@@ -13,11 +18,11 @@ public class IndexController {
 	@Autowired
 	private SquadraService squadreService;
 	
-//	@Autowired
-//	private CredenzialiService credenzialiService;
-//	
-//	@Autowired
-//	private GestoreSquadraService gestoreSquadraService;
+	@Autowired
+	private CredenzialiService credenzialiService;
+	
+	@Autowired
+	private GestoreSquadraService gestoreSquadraService;
 	
 	@GetMapping(value = "/")
 	public String getIndexPage(Model model) {
@@ -28,9 +33,9 @@ public class IndexController {
 	@GetMapping(value = "/dashboard")
 	public String getDashboardPage(Model model) {
 		//va cambiato
-//		UserDetails user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//		GestoreSquadra manager = this.gestoreSquadraService.findGestoreSquadraByCredenziali(this.credenzialiService.findCredenzialiByUsername(user.getUsername()));
-		model.addAttribute("squadre", this.squadreService.findSquadreIdGestoreSquadra((long) 3));
+		UserDetails user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		GestoreSquadra manager = this.gestoreSquadraService.findGestoreSquadraByCredenziali(this.credenzialiService.findCredenzialiByUsername(user.getUsername()));
+		model.addAttribute("squadre", this.squadreService.findSquadreIdGestoreSquadra(manager.getIdGestoreSquadra()));
 		return "dashboard.html";
 	}
 }
