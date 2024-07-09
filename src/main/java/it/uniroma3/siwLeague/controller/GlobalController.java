cffc15640dedeebf616ded949f5bacd5f436e2d1
ale0699_PCLaptop <atulli030@gmail.com>
2024-07-09 18:42:26 +0200

    Update GlobalController.java

--- src/main/java/it/uniroma3/siwLeague/controller/GlobalController.java
+++ src/main/java/it/uniroma3/siwLeague/controller/GlobalController.java
@@ -1,5 +1,6 @@
 package it.uniroma3.siwLeague.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,9 +8,14 @@ import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import it.uniroma3.siwLeague.model.Credenziali;
+import it.uniroma3.siwLeague.service.CredenzialiService;
+
 @ControllerAdvice
 public class GlobalController {
 	
+	@Autowired
+	private CredenzialiService credenzialiService;
 	
 	@ModelAttribute("username")
 	public String getUsername() {
@@ -35,5 +41,31 @@ public class GlobalController {
 			}
 		}
 	}	
+	
+	@ModelAttribute("userRole")
+	public String getRuolo() {
+
+	    if (SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken) {
+	        return null;
+	    } else {
+	        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+	        Credenziali credenziali = null;
+
+	        if (principal instanceof UserDetails) {
+	            UserDetails user = (UserDetails) principal;
+	            credenziali = this.credenzialiService.findCredenzialiByUsername(user.getUsername());
+	        } else if (principal instanceof DefaultOAuth2User) {
+	            DefaultOAuth2User user = (DefaultOAuth2User) principal;
+	            credenziali = this.credenzialiService.findCredenzialiByUsername(user.getAttribute("email"));
+	        }
+
+	        if (credenziali != null) {
+	            return credenziali.getRuolo().toLowerCase();
+	        } else {
+
+	            return null;
+	        }
+	    }
+	}
 	
 }
